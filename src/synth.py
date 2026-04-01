c0649fabdb10fb499780e9747b39100b14dfa03e
AnnelineD <anneline.daggelinckx@hotmail.com>
2026-04-01 14:07:49 -0500

    Add dot + refactor unfinished to active

--- src/synth.py
+++ src/synth.py
@@ -20,7 +20,7 @@ class Graph:
             print(f"\tif state == '{state.name}':")
             for t in self.transitions:
                 if t.s_from != state: continue
-                if t.unfinished: print(f"\t\tif {' and '.join(f'tmp_{c.name}' for c in t.unfinished)}:")
+                if t.active: print(f"\t\tif {' and '.join(f'tmp_{c.name}' for c in t.active)}:")
                 elif t.finished: print(f"\t\tif {' and '.join(f'not tmp_{c.name}' for c in t.finished)}:")
                 else: print(f"\t\tif True:")
                 if t.when:
@@ -37,7 +37,26 @@ class Graph:
             print("\t\tprint(state, 'not continued!')")
             print("\t\tbreak")
     def dot(self):
-        ...
+        label_index = 0
+        for state in self.vtcs:
+            print(state.name, ";")
+        for t in self.transitions:
+            temp = []
+            for src in t.active: temp.append(f"active {src.name}")
+            for src in t.finished: temp.append(f"finished {src.name}")
+            if t.when:
+                for c in t.when:
+                    if isinstance(c, Inequality): temp.append(f'{c.lhs.name} {c.kind} {c.rhs.name}')
+                    if isinstance(c, OpOrNot): temp.append(f'({c.lhs.name} is None or {c.lhs.name} {c.kind} {c.rhs.name})')
+
+            for dst, src in t.push: temp.append(f"{dst.name}!{src.name}")
+            for src in t.pull: temp.append(f"{src.name}.pull()")
+            label = f"{' \\n '.join(temp)}"
+            print(f"l{label_index} [shape=\"rect\", label=\"{label}\"];")
+            print(f"{t.s_from.name} -> l{label_index} [arrowhead=\"none\"];")
+            print(f"l{label_index} -> {t.s_to.name};")
+            label_index += 1
+
 class Node:
     def __init__(self, graph, name): self.graph = graph; self.name = name
     @classmethod
@@ -57,11 +76,11 @@ class OpOrNot(Cond):
     def __init__(self, kind: Literal["=="] | Literal["<"] | Literal[">"] | Literal[">="] | Literal["<="], lhs: Src, rhs: Src):
         self.kind = kind; self.lhs = lhs; self.rhs = rhs
 class Transition:
-    def __init__(self, s_from, s_to, when, push, pull, unfinished, finished):
-        self.s_from = s_from; self.s_to = s_to; self.when = when; self.push = push; self.pull = pull; self.unfinished = unfinished; self.finished = finished;
+    def __init__(self, s_from, s_to, when, push, pull, active, finished):
+        self.s_from = s_from; self.s_to = s_to; self.when = when; self.push = push; self.pull = pull; self.active = active; self.finished = finished;
 class Vtx(Node):
-    def to(self, other, *when, push=(), pull=(), unfinished=(), finished=()):
-        self.graph.transitions.append(Transition(self, other, when, push, pull, unfinished, finished))
+    def to(self, other, *when, push=(), pull=(), active=(), finished=()):
+        self.graph.transitions.append(Transition(self, other, when, push, pull, active, finished))
 
 def ctx():
 
@@ -111,13 +130,13 @@ def intersection_graph():
 
     s0.to(s1, pull=(a, b, c)) # s0
 
-    s1.to(s1, a < b, pull=(a, ), unfinished=(a, b, c))
-    s1.to(s1, a < c, pull=(a, ), unfinished=(a, b, c))
-    s1.to(s1, b < a, pull=(b, ), unfinished=(a, b, c))
-    s1.to(s1, b < c, pull=(b, ), unfinished=(a, b, c))
-    s1.to(s1, c < a, pull=(c, ), unfinished=(a, b, c))
-    s1.to(s1, c < b, pull=(c, ), unfinished=(a, b, c))
-    s1.to(s1, a == b, b ==c, push=((r, a), ), pull=(a, b, c), unfinished=(a, b, c))
+    s1.to(s1, a < b, pull=(a, ), active=(a, b, c))
+    s1.to(s1, a < c, pull=(a, ), active=(a, b, c))
+    s1.to(s1, b < a, pull=(b, ), active=(a, b, c))
+    s1.to(s1, b < c, pull=(b, ), active=(a, b, c))
+    s1.to(s1, c < a, pull=(c, ), active=(a, b, c))
+    s1.to(s1, c < b, pull=(c, ), active=(a, b, c))
+    s1.to(s1, a == b, b ==c, push=((r, a), ), pull=(a, b, c), active=(a, b, c))
 
     return g
 
@@ -128,32 +147,24 @@ def ctx2():
     g = Graph()
     a, b, c, d = g.sources('a', 'b', 'c', 'd')
     r, = g.sinks('r')
-    s0, s1, s11, s15, s19 = g.states('s0', 's1', 's11', 's15', 's19')
+    s0, s1, s2, s3 = g.states('s0', 's1', 's2', 's3')
     g.init = s0
 
     s0.to(s1, pull=(a, b, c, d)) # s0
 
-    s1.to(s1, a < c, b < c, pull=(a, b)) # s2
-    s1.to(s1, a < c, b == c, pull=(a,)) # s3
-    s1.to(s1, a < c, b > c, pull=(a,)) # s4
-    s1.to(s1, a == c, b < c, pull=(b,)) # s5
-    s1.to(s1, a > c, b < c, pull=(b,)) # s8
-    s1.to(s1, a > c, b > c, pull=(c,)) # s10
-    s1.to(s11, a == c, b == c, pull=()) # s6
-    s1.to(s15, a == c, b > c, pull=()) # s7
-    s1.to(s19, a > c, b == c, pull=()) # s9
+    s1.to(s1, a < c, pull=(a, ), active=(a, c))
+    s1.to(s1, b < c, pull=(b, ), active=(b, c))
+    s1.to(s2, a == c, active=(a, c))
+    s1.to(s3, b == c, active=(b, c))
+    s1.to(s1, OpOrNot(">", a, c), OpOrNot(">", b, c), pull=(c,), active=(c, ))  # s10
 
-    s11.to(s11, a > d, b > d, pull=(d,)) # s14
-    s11.to(s1, a == d, b == d, pull=(a, b, c)) # s13
-    s11.to(s1, a < d, b < d, push=((r, a),), pull=(a, b)) # s12
+    s2.to(s2, a > d, pull=(d,), active=(a, d)) # s14
+    s2.to(s1, a == d, pull=(a, ), active=(a, d))
+    s2.to(s1, OpOrNot(">", d, a), push=((r, a),), pull=(a, )) # s12
 
-    s15.to(s15, a > d, pull=(d,)) # s18
-    s15.to(s1, a == d, pull=(a,)) # s17
-    s15.to(s1, a < d, push=((r, a),), pull=(a,)) # s16
-
-    s19.to(s19, b > d, pull=(d,)) # s22
-    s19.to(s1, b == d, pull=(b,)) # s21
-    s19.to(s1, b < d, push=((r, b),), pull=(b,)) # s20
+    s3.to(s3, b > d, pull=(d,), active=(b, d)) # s14
+    s3.to(s1, b == d, pull=(b, ), active=(b, d))
+    s3.to(s1, OpOrNot(">", d, b), push=((r, b),), pull=(b, )) # s12
 
     return g
 
@@ -179,12 +190,12 @@ class Sink:
         self.data.append(inp)
 
 if __name__ == '__main__':
-    # g = ctx()
-    g = intersection_graph()
+    g = ctx2()
+    # g = intersection_graph()
     g.py()
 
-    # a = Source('a', {'1', 'B', 'C'})
-    a = Source('a', {'1', 'B', 'C', 'A', '3'})
+    a = Source('a', {'1', 'B', 'C'})
+    # a = Source('a', {'1', 'B', 'C', 'A', '3'})
     b = Source('b', {'2', '3', 'A'})
     c = Source('c', {'3', '4', 'A', 'B'})
     d = Source('d', {'1', 'A'})
@@ -197,3 +208,4 @@ if __name__ == '__main__':
     except IndexError: print("stopped by exhaustion")
 
     print("result", r.data)
+    g.dot()
